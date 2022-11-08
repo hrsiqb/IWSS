3bae75326d1b021c6c6b711f3a0169898cedae5e
DESKTOP-HIRTIRG\haris <hrsiqb@gmail.com>
2022-11-09 00:44:06 +0500

    second commit

--- firebase-config/firebase-config.js
+++ firebase-config/firebase-config.js
@@ -3,17 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log('here')
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8CnMde-HaQKebMU994Xt7rI3IkihnKq8",
-  authDomain: "thedimensionspvtltd.firebaseapp.com",
-  projectId: "thedimensionspvtltd",
-  storageBucket: "thedimensionspvtltd.appspot.com",
-  messagingSenderId: "969548320091",
-  appId: "1:969548320091:web:bc5949627d3b6ad50cde7e",
-  measurementId: "G-Y8600M9TCK"
+  apiKey: "AIzaSyDMuBmoM6pxgt1dmDT8enXPYI31tBYZ-fc",
+  authDomain: "iwss-llc.firebaseapp.com",
+  projectId: "iwss-llc",
+  storageBucket: "iwss-llc.appspot.com",
+  messagingSenderId: "221423893828",
+  appId: "1:221423893828:web:d70cd0680f69399563eb11",
+  measurementId: "G-3ZWEPFSX8V"
 };
 
 // Initialize Firebase
